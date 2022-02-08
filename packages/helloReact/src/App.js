f544ec5517e67d784eeb05feb05261ca55a04ab4
rajdatta <rajdatta.bhosale@gmail.com>
2022-02-08 10:13:40 +0000

    some

--- packages/helloReact/src/App.js
+++ packages/helloReact/src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Switch, Route, Router, Link } from 'react-router-dom'
+import React from "react";
+import { Switch, Route, Router, Link } from "react-router-dom";
 
-const helloWorld = () => (<div>Hello World!</div>)
-const helloReact = () => (<div>Hello React!</div>)
+const helloWorld = () => <div>Hello World!</div>;
+const helloReact = () => <div>Hello React!</div>;
 
 export default ({ history }) => {
-    return <div>
-        <Router history={history}>
-            <Switch>
-                <Route path="/react" component={helloReact} />
-                <Route path="/" component={helloWorld} />
-            </Switch>
-            <br />
-            <Link to='/react'>Say hello to React**!</Link>
-            <br />
-            <Link to='/'>Say hello to the World!</Link>
-        </Router>
+  return (
+    <div>
+      <Router history={history}>
+        <Switch>
+          <Route path="/react" component={helloReact} />
+          <Route path="/" component={helloWorld} />
+        </Switch>
+        <br />
+        <Link to="/react">Router based navigation to React path</Link>
+        <br />
+        <Link to="/">Say hello to the World!</Link>
+      </Router>
     </div>
-}
+  );
+};

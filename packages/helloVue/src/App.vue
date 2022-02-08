f20211d9b503ab027ec1c73d82fc09030ea93971
rajdatta <rajdatta.bhosale@gmail.com>
2022-02-08 20:16:17 +0000

    some

--- packages/helloVue/src/App.vue
+++ packages/helloVue/src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
     {{ message }}
-    <h1>Header 1</h1>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return { message: "Hello  ** Vue ****" };
+    return { message: "*** Hello Vue" };
   },
 };
 </script>
